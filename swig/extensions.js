23d3dff50f0b6b5580e296b477310b1c363d5b15
Max Barry <maxbarrymail@gmail.com>
2015-01-26 11:59:42 +0000

    Update extensions.js

--- swig/extensions.js
+++ swig/extensions.js
@@ -12,7 +12,16 @@ filters.stringToArray = function(str) {
     return str.split(",");
 };
 
-
+filters.combineArrays = function(primary, secondary) {
+    /**
+    Combine two arrays in to a single array.
+    
+    @param {array} primary First array
+    @param {array} secondary Second array
+    @return {array} Primary array + secondary array
+    */
+    return primary.concat(secondary);
+};
 
 module.exports.register = function (swig, opts) {
   opts = opts || {};
