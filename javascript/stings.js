03a074a2c4084f83a106db1631eff184707bce2d
Max Barry <maxbarrymail@gmail.com>
2015-06-28 23:14:45 +0100

    Added slugify function

--- javascript/stings.js
+++ javascript/stings.js
@@ -1,7 +1,27 @@
 /**
-Check if "data" starts with "str"
+Check if the provided input starts with a given string
 
+@param {string} input
+@param {string} string
 @link http://stackoverflow.com/a/4579228
-@returns {bool} True if "data" starts with "str"
+@returns {bool} True if "input" starts with "string"
 */
-data.lastIndexOf(str, 0) === 0
+function contains_string (input, string) {
+  return input.lastIndexOf(string, 0) === 0;
+}
+
+/**
+Slugify a given string
+
+@param {string} text
+@link https://gist.github.com/mathewbyrne/1280286
+@returns {string}
+*/
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
