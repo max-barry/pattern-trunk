961713130d39e026253e288ab2dacebca12762b7
Max Barry <maxbarrymail@gmail.com>
2015-12-01 16:42:48 +0000

    Added isElementInViewport

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -8,3 +8,26 @@ function reached_floor(distance) {
     distance = distance || 350;
     return ($(document).height() - ($(window).height() + $(window).scrollTop())) < distance;
 }
+
+/**
+Checks if an element is inside of the viewport
+
+@link http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
+@example See link (above)
+*/
+function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
